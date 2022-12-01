aa9a49652ec81a7e5b44c976f264a8067d18d3b2
Gasteri <tipzov@yandex.ru>
2022-12-01 18:41:44 +0300

    weekday

---
+++ task2/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Напишите программу, которая принимает на вход цифру,
+обозначающую день недели, и проверяет, 
+является ли этот день выходным.6 -> да, 7 -> да, 1 -> нет. */
+
+Console.WriteLine("Введите цифру от 1 до 7");
+bool isParset = int.TryParse(Console.ReadLine(), out int number);
+if(!isParset)
+{
+    Console.WriteLine("Не является числом");
+    return;
+}
+
+if(number == 6 | number == 7)
+{
+    Console.WriteLine($"{number} -> Да");
+}
+else
+{
+    Console.WriteLine($"{number} -> Нет");
+}
