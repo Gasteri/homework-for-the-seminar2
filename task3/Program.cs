d6381f78284a50e9b3d28d529aa02fe68c983faa
Gasteri <tipzov@yandex.ru>
2022-12-01 23:59:19 +0300

    output of the third digit

---
+++ task3/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+что третьей цифры нет. Не использовать string[]. 645 -> 5, 78 -> третьей цифры нет, 32679 -> 6. */
+
+Console.WriteLine("Введите произвольное число");
+bool isParset = int.TryParse(Console.ReadLine(), out int number);
+if(!isParset)
+{
+    Console.WriteLine("Не является числом");
+    return;
+}
+int deletingdigit = number;
+if(deletingdigit < 100)
+    {
+        Console.WriteLine($"{number} -> третьей цифры нет");
+        return;
+    }
+    while(deletingdigit >= 1000)
+        {
+            deletingdigit = deletingdigit / 10;    
+        } 
+Console.WriteLine($"{number} -> {deletingdigit = (deletingdigit % 10)}");
+
+
+
+
+
+
